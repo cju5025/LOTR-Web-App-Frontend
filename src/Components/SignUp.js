10dbc265d790074bb5e8575dd6c85c7e6f7858dc
Colter Ulrich <cju5025@gmail.com>
2021-11-25 20:44:41 -0700

    Controls SignUp Form

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,6 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
 function SignUp () {
+
+    const [ username, setUsername ] = useState( " " )
+    const [ password, setPassword ] = useState( " " )
+    const [ passwordVerification, setPasswordVerification ] = useState( " " )
+    const [ passwordsMatch, setPasswordsMatch ] = useState( false )
+
     return (
-        <h1>Sign Up</h1>
+        <div id="sign-up">
+            <form id="sign-up-form">
+                <label>Username:</label>
+                <input 
+                    type="text" 
+                    name="username" 
+                    placeholder="username" 
+                    onChange={ e => setUsername(e.target.value) }
+                >
+                </input>
+
+                <label>Password: </label>
+                <input 
+                type="password" 
+                name="password" 
+                placeholder="password" 
+                onChange={ e => setPassword(e.target.value )}
+                >
+                </input>
+
+                <label>Verify Password:</label>
+                <input 
+                type="password" 
+                name="password-verification" 
+                placeholder="" 
+                onChange={ e => setPasswordVerification(e.target.value) }
+                >   
+                </input>
+            </form>
+        </div>
     )
 }
 
